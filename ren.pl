413e7861e064a9e2d938e5438e4ef0f851fb835c
Sébastien Millet <sebastien.millet@gmail.com>
2023-10-22 20:46:32 +0200

    pre-alpha - manages duplicate names

--- ren.pl
+++ ren.pl
@@ -107,8 +107,81 @@ sub dbg() {
     print @_, "\n";
 }
 
+# Equivalent to '-f' but case insensitive no matter the OS.
+# An optional, third parameter is to activate use of cached data.
+# USE THIS THIRD PARAMETER WITH CAUTION!
+#   When using cached data, consecutive calls for the same directory will rely
+#   on cached data (only the first call will read directory content).
+#   That means, file operations done in the directory in-between won't be
+#   reflected.
+sub check_file_exists_case_insensitive() {
+    state $cached_dir_name = undef;
+    state %cached_dir_hash = ();
+
+    my ( $dir, $file, $use_cache ) = @_;
+
+    print "dir: [$dir], file: [$file]\n";
+
+    $use_cache //= 0;
+
+    if (   ( !defined $cached_dir_name )
+        or ( ( $cached_dir_name // '' ) ne $dir )
+        or ( !$use_cache ) )
+    {
+        %cached_dir_hash = ();
+
+        # Yes, we store undef values in the hash, as we don't need more.
+        # The value doesn't matter, all we want to know later is, whether or
+        # not the key exists.
+        undef $cached_dir_hash{ lc($_) } for (<${dir}*>);
+        $cached_dir_name = $dir;
+
+        #        print STDERR "** READ DIRECTORY **\n";
+    }
+    else {
+        #        print STDERR "** use cached directory data **\n";
+    }
+
+    return exists $cached_dir_hash{ lc($file) };
+}
+
+# Convert an integer number into a 'special base-26-based' representation using
+# symbols 'a' to 'z'.
+# That is, convert
+#   0 to 'a'
+#   1 to 'b'
+#   ...
+#   25 to 'z'
+#   26 to 'aa'
+#   27 to 'ab'
+#   ...
+#   and so on
+# The special value -1 (-1 is the only negative value allowed) returns an empty
+# string.
+sub int_to_letters {
+    my $n = shift;
+
+    die "Negative value (except for -1) not allowed ($n)" if $n < -1;
+    die "Non-integer value not allowed ($n)"              if int($n) != $n;
+
+    return '' if $n == -1;
+
+    my $ret = '';
+    do {
+        $ret = chr( ord('a') + ( $n % 26 ) ) . $ret;
+        $n   = int( $n / 26 ) - 1;
+    } while ( $n >= 0 );
+    return $ret;
+}
+
+#for (my $i = -1; $i < 800; $i++) {
+#    print "$i -> ", &int_to_letters($i), "\n";
+#}
+#exit;
+
 &dbg("Processing directory '$directory_to_process'");
 
+my %targets;
 my @rename_list;
 my $glob_pattern = catfile( $directory_to_process, '*' );
 while ( my $file = glob($glob_pattern) ) {
@@ -153,21 +226,44 @@ while ( my $file = glob($glob_pattern) ) {
         next;
     }
 
+    my $raw_file = catfile( $dir, $base ) . $RAW_EXT;
+    my $has_raw_file =
+      &check_file_exists_case_insensitive( $dir, $raw_file, 1 );
+
     my $new_name_base = $image_date_obj->strftime($NEW_NAME_BASE_FORMAT);
 
-    my $file_target_name = catfile( $dir, $new_name_base ) . $ext;
+    my $file_target_name;
+    my $raw_file_target_name;
+    my $dedup_int = -1;
+    my $file_target_exists;
+    my $raw_target_exists;
+    do {
+        my $dedup_postfix = &int_to_letters($dedup_int);
+
+        $file_target_name =
+          catfile( $dir, $new_name_base ) . $dedup_postfix . $ext;
+        $file_target_exists =
+          &check_file_exists_case_insensitive( $dir, $file_target_name )
+          || exists $targets{$file_target_name};
+
+        $raw_target_exists = 0;
+        if ($has_raw_file) {
+            $raw_file_target_name =
+              catfile( $dir, $new_name_base ) . $dedup_postfix . $RAW_EXT;
+            $raw_target_exists =
+              &check_file_exists_case_insensitive( $dir, $raw_file_target_name )
+              || exists $targets{$raw_file_target_name};
+        }
+
+        $dedup_int++;
+
+    } while ( $file_target_exists or $raw_target_exists );
+
+    undef $targets{$file_target_name};
+    undef $targets{$raw_file_target_name} if $has_raw_file;
 
     &dbg("   New name base: $new_name_base");
     &dbg("   Target:        $file_target_name");
-
-    my $raw_file     = catfile( ${dir}, ${base} ) . ${RAW_EXT};
-    my $has_raw_file = -f $raw_file;
-
-    my $raw_file_target_name;
-    if ($has_raw_file) {
-        $raw_file_target_name =
-          catfile( ${dir}, ${new_name_base} ) . ${RAW_EXT};
-    }
     &dbg( "   Raw name:      $raw_file ",
         ( $has_raw_file ? '(exists)' : '(does not exist)' ) );
     if ($has_raw_file) {
@@ -182,27 +278,13 @@ while ( my $file = glob($glob_pattern) ) {
 
 &dbg('');
 
-#for my $elem (@rename_list) {
-#    print "$$elem[0] -> $$elem[1]\n";
-#}
-
-# Convert an integer number into a base-26 representation using symbols 'a' to 'z'.
-# That is, convert 0 to 'a', 1 to 'b', 25 to 'z', 26 to 'aa', 27 to 'ab', and so on.
-sub int_to_letters {
-    my $n = shift;
-
-    die "Negative value not allowed ($n)" if $n < 0;
-
-    my $ret = '';
-    my $fact = 0;
-    do {
-        $ret = chr(ord('a') + ($n % 26)) . $ret;
-        $n = int($n / 26);
-    } while ($n >= 1);
-    return $ret;
+for my $elem (@rename_list) {
+    print "$$elem[0] -> $$elem[1]\n";
 }
 
-for (my $i = 0; $i < 800; $i++) {
-    print "$i -> ", &int_to_letters($i), "\n";
-}
+my $s0 = $rename_list[0][0];
+my $d0 = $rename_list[0][1];
+say $s0;
+say $d0;
 
+#rename $s0, $d0
